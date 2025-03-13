1ec62a49ca1beff53cfc19985607680220d616a5
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-13 02:53:59 -0600

    initial commit. grid setup complete.

---
+++ 31_Challenge_FountainOfObjects/Cell.cs
@@ -0,0 +1,52 @@
+﻿namespace _31_Challenge_FountainOfObjects;
+
+// Holds Coordinates
+public class Cell
+{
+    public int Row { get; }
+    public int Column { get; }
+
+    public Cell(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public override string ToString() => $"({Row},{Column})";
+}
+
+// Setup 2D array of Cells
+public class Grid
+{
+    public int Rows { get; }
+    public int Columns { get; }
+    public Cell[,] Cells { get; }
+
+    public Grid(int rows, int columns)
+    {
+        Rows = rows;
+        Columns = columns;
+        Cells = new Cell[Rows, Columns];
+
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Columns; col++)
+            {
+                Cells[row, col] = new Cell(row, col);
+            }
+        }
+    }
+
+    public void Display()
+    {
+        for (int row = 0; row < Rows; row++)
+        {
+            Console.Write(" | ");
+            for (int col = 0; col < Columns; col++)
+            {
+                Console.Write(Cells[row, col].ToString() + " | ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 31_Challenge_FountainOfObjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _31_Challenge_FountainOfObjects;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Grid mainGrid = new Grid(5, 5);
+        mainGrid.Display();
+    }
+}
